2a2a3512e638852a5af0eabca77477e5b4b19215
Nazym Jumadilova <n.jumadilova@gmail.com>
2026-03-08 02:05:57 +0100

    Update styling

---
+++ src/components/Stack.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let direction = 'row';
+	export let gap = '0.5rem';
+	export let align = 'center';
+	export let justify = 'flex-start';
+</script>
+
+<div
+	class="stack"
+	style="--direction:{direction}; --gap:{gap}; --align:{align}; --justify:{justify};"
+>
+	<slot />
+</div>
+
+<style>
+	.stack {
+		display: flex;
+		flex-direction: var(--direction);
+		gap: var(--gap);
+		align-items: var(--align);
+		justify-content: var(--justify);
+	}
+</style>
